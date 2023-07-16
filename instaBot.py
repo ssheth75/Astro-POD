983cf6d1fdfec10983f08f3219b8c8402d481ba1
Shirvil Sheth <96599193+ssheth75@users.noreply.github.com>
2023-07-16 16:54:32 -0400

    Implement credits scraping

--- instaBot.py
+++ instaBot.py
@@ -10,30 +10,35 @@ import os
 import re
 
 
+
 ### Delete config file from directory to prevent future login issues ###
-config_folder = ''
+config_folder = '/Users/shirvilsheth/Documents/Personal Projects/Astro-POD/config'
 
 # Remove the config folder if it exists
 if os.path.exists(config_folder):
     shutil.rmtree(config_folder)
 
-### Get image from NASA ###
 
+
+
+### Get image from NASA ###
 url = "https://apod.nasa.gov/apod/astropix.html"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
 image = soup.find("img")
-# image = images[0]
-image_src = image["src"]
+imageSrc = image["src"]
 
-# Check if the image_src URL has a scheme and add it if missing
-if not image_src.startswith('http'):
-    image_src = urljoin(url, image_src)
+# Check if the imageSrc URL has a scheme and add it if missing
+if not imageSrc.startswith('http'):
+    imageSrc = urljoin(url, imageSrc)
 
-response = requests.get(image_src)
+response = requests.get(imageSrc)
 with open("post.jpg", "wb") as file:
     file.write(response.content)
+
+
+
 
 ### Crop or resize the image for compatibility with Instagram ###
 inputImagePath = "post.jpg"
@@ -41,19 +46,32 @@ outputImagePath = "formattedPost.jpg"
 targetSize = 1080
 processImage.cropOrResizeimage(inputImagePath, outputImagePath, targetSize)
 
-### Obtain Image title ###
+
+
+
+### Obtain Image title and credits ###
 imageTitle = ""
+credits = []
 centerTags = soup.find_all("center")
 
 if len(centerTags) >= 2:
     targetCenterTag = centerTags[1]
     imageTitle = targetCenterTag.find("b").text
     print(imageTitle)
+
+    # Obtain the credits
+    targetATags = targetCenterTag.find_all("a")
+
+    for targetATag in targetATags:
+        credits.append(targetATag.text)
+    print(credits)
+
 else:
     print("Image title not found.")
     exit(1)
 
 formattedImageTitle = imageTitle.lstrip()
+
 
 
 
@@ -82,9 +100,23 @@ else:
     exit(1)
 
 
+
+
 ### Format Caption ###
 currentDate = datetime.now().strftime("%m/%d/%Y")
-formattedCaption = "ASTRO Picture of The Day: " + currentDate + "\n\n" + formattedImageTitle + "\n\n" + formattedText + "\n\n" + "Credit: NASA"
+
+creditsString = ""
+if len(credits) > 0:
+    creditsString = ", ".join(credits)
+    print(creditsString)
+else:
+    creditsString = "NASA"
+
+formattedCaption = "ASTRO Picture of The Day: " + currentDate + "\n\n" + formattedImageTitle + "\n\n" + formattedText + "\n\n" + "Image Credit & Copyright: NASA, " + creditsString
+print(formattedCaption)
+
+
+
 
 ### Login to Instagram ###
 bot = Bot()
